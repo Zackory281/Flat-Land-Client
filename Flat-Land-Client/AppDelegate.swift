8c29950d1e6988227dac114f5f5f20e3bf1cfb52
Zackory Cramer <zackory137c@gmail.com>
2018-07-19 11:16:13 -0700

    Initial Commit

---
+++ Flat-Land-Client/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Flat-Land-Client
+//
+//  Created by Zackory Cramer on 7/19/18.
+//  Copyright © 2018 Zackori Cui. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Flat-Land-Client/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Flat-Land-Client
+//
+//  Created by Zackory Cramer on 7/19/18.
+//  Copyright © 2018 Zackori Cui. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
